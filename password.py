d432c446b726fbc54dfbbe7f65d7edc9261c1b3f
Md Anwar Hossain <anwar.hossain5854@gmail.com>
2022-08-17 03:35:11 +0530

    Credential is set as environment path

--- password.py
+++ password.py
@@ -4,3 +4,13 @@ username = input("Enter username: ")
 password = getpass("Enter password: ")
 
 print("Logging in.....")
+
+#*********************************************
+
+import os
+
+user_id=os.environ.get("USER_ID")
+user_pass=os.environ.get("USER_PASS")
+
+print(user_id)
+print(user_pass)
