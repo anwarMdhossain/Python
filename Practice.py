83e8f20c4c9fc66312fe084bcab9e329d14c6b0e
Md Anwar Hossain <anwar.hossain5854@gmail.com>
2022-09-05 04:57:30 +0530

    Practice file update

--- Practice.py
+++ Practice.py
@@ -1,4 +1,17 @@
 #practice
+#to print Exception class tree
+def print_exception_tree(thisclass, nest = 0):
+    if nest > 1:
+        print("   |" * (nest - 1), end="")
+    if nest > 0:
+        print("   +---", end="")
+
+    print(thisclass.__name__)
+
+    for subclass in thisclass.__subclasses__():
+        print_exception_tree(subclass, nest + 1)
+
+print_exception_tree(BaseException)
 
 #yield is similar as return being only difference as it returns a generator
 #which generates one value at a time based on requirement
